b796d1531c4ffae3530bcf94b63bdff0c7419dd2
Woosung_Kim <sibu2005@naver.com>
2023-03-26 01:07:32 +0900

    컨트롤러 및 MockMvc를 사용한 컨트롤러 테스트

---
+++ src/test/java/com/woosung/prj/springboot/web/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package com.woosung.prj.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class HelloControllerTest {
+
+    @Autowired private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
