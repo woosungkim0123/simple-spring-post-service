66ed9b93f4859cf9713e325f14edf4ffa9ccb2c5
Woosung_Kim <sibu2005@naver.com>
2023-03-27 16:35:23 +0900

    post save, findAll test

---
+++ src/test/java/com/woosung/prj/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.woosung.prj.springboot.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired PostsRepository postsRepository;
+
+    @Test
+    public void 게시글저장_불러오기() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("woosung@gmail.com")
+                .build()
+        );
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        System.out.println("postsList.size() = " + postsList.size());
+        // then
+        Posts posts = postsList.get(0);
+        
+        System.out.println("posts.getId() = " + posts.getId());
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+}
