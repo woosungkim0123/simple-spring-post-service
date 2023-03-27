effce5c5c72b6753e5010888d9fecb4539b8c019
Woosung_Kim <sibu2005@naver.com>
2023-03-28 00:08:08 +0900

    테스트 Mock으로 해결

--- src/test/java/com/woosung/prj/springboot/web/HelloControllerTest.java
+++ src/test/java/com/woosung/prj/springboot/web/HelloControllerTest.java
@@ -1,8 +1,12 @@
 package com.woosung.prj.springboot.web;
 
+import com.woosung.prj.springboot.service.PostsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.is;
 
@@ -13,6 +17,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class HelloControllerTest {
 
     @Autowired private MockMvc mvc;
+    @MockBean private PostsService postsService;
+    @MockBean private JpaMetamodelMappingContext jpaMetamodelMappingContext;
 
     @Test
     public void hello가_리턴() throws Exception {
