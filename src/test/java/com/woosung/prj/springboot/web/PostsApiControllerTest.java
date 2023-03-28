b4dc4b981bba5acf76e28fabb2e75196605a50b7
Woosung_Kim <sibu2005@naver.com>
2023-03-28 13:55:10 +0900

    테스트 수정

--- src/test/java/com/woosung/prj/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/woosung/prj/springboot/web/PostsApiControllerTest.java
@@ -3,14 +3,19 @@ package com.woosung.prj.springboot.web;
 import com.woosung.prj.springboot.domain.posts.Posts;
 import com.woosung.prj.springboot.domain.posts.PostsRepository;
 import com.woosung.prj.springboot.web.dto.PostsSaveRequestDto;
+import com.woosung.prj.springboot.web.dto.PostsUpdateRequestDto;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -27,6 +32,11 @@ class PostsApiControllerTest {
     private TestRestTemplate restTemplate;
     @Autowired
     private PostsRepository postsRepository;
+
+    @AfterEach
+    public void tearDown() {
+        postsRepository.deleteAll();
+    }
 
     @Test
     public void Posts_등록된다() {
@@ -46,5 +56,42 @@ class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void Posts_수정된다() {
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        System.out.println("savedPosts = " + savedPosts);
+
+        Long updateId = savedPosts.getId();
+        System.out.println("updateId = " + updateId);
+
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+        
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
 }
