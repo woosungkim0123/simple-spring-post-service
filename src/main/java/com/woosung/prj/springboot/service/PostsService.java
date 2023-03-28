9e5f9af28660c2c6fea29802aebe70b56e137b0e
Woosung_Kim <sibu2005@naver.com>
2023-03-28 23:01:04 +0900

    글 등록 수정 삭제 화면 및 기능 종료

--- src/main/java/com/woosung/prj/springboot/service/PostsService.java
+++ src/main/java/com/woosung/prj/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.woosung.prj.springboot.service;
 
 import com.woosung.prj.springboot.domain.posts.Posts;
 import com.woosung.prj.springboot.domain.posts.PostsRepository;
+import com.woosung.prj.springboot.web.dto.PostsListResponseDto;
 import com.woosung.prj.springboot.web.dto.PostsResponseDto;
 import com.woosung.prj.springboot.web.dto.PostsSaveRequestDto;
 import com.woosung.prj.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id =" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,6 +4,12 @@ const index = {
         $('#btn-save').on('click', function () {
             _this.save();
         })
+        $('#btn-update').on('click', function () {
+            _this.update();
+        })
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        })
     },
     save: function () {
         const data = {
@@ -23,7 +29,39 @@ const index = {
         }).fail(function(error) {
             alert(JSON.stringify(error));
         })
-
+    },
+    update: function () {
+        const data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+        const id = $("#id").val();
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        })
+    },
+    delete: function() {
+        const id = $("#id").val();
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        })
     }
 }
 
