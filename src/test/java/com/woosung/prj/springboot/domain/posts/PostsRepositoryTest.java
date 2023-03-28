10003299704a43de278183c282a23334615d3861
Woosung_Kim <sibu2005@naver.com>
2023-03-28 11:38:11 +0900

    cleanup 메서드추가

--- src/test/java/com/woosung/prj/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/woosung/prj/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,20 +1,27 @@
 package com.woosung.prj.springboot.domain.posts;
 
-import org.assertj.core.api.Assertions;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 class PostsRepositoryTest {
 
     @Autowired PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
 
     @Test
     public void 게시글저장_불러오기() {
@@ -31,12 +38,29 @@ class PostsRepositoryTest {
 
         // when
         List<Posts> postsList = postsRepository.findAll();
+
         // then
         Posts posts = postsList.get(0);
-
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
 
+    @Test
+    public void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
 }
