b95380aa886c813f3e9e03f5a7ad78172b477d53
Woosung_Kim <sibu2005@naver.com>
2023-03-28 16:14:36 +0900

    인덱스 페이지 추가 및 테스트

---
+++ src/main/java/com/woosung/prj/springboot/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.woosung.prj.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("item", "하이염");
+        return "index";
+    }
+}
---
+++ src/test/java/com/woosung/prj/springboot/web/IndexControllerTest.java
@@ -0,0 +1,26 @@
+package com.woosung.prj.springboot.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() {
+        // when
+        String body = restTemplate.getForObject("/", String.class);
+
+        // then
+        assertThat(body).contains("스프링 부트로 CI/CD 배포까지");
+    }
+}
