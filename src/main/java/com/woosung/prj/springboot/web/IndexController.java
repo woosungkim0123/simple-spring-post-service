f6dd6072de8a4d9300d62f727950ab6cf54a34d6
Woosung_Kim <sibu2005@naver.com>
2023-03-28 20:18:16 +0900

    타임리프 layout 추가 및 화면

--- src/main/java/com/woosung/prj/springboot/web/IndexController.java
+++ src/main/java/com/woosung/prj/springboot/web/IndexController.java
@@ -1,15 +1,18 @@
 package com.woosung.prj.springboot.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("item", "하이염");
+    public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
