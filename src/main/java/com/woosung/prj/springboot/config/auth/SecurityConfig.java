63141f9931bc10d682e6e127e0794dc3856d5a90
Woosung_Kim <sibu2005@naver.com>
2023-03-29 15:57:50 +0900

    로그인 기능

---
+++ src/main/java/com/woosung/prj/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.woosung.prj.springboot.config.auth;
+
+import com.woosung.prj.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .headers().frameOptions().disable()
+            .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+            .and()
+                .logout()
+                    .logoutSuccessUrl("/")
+            .and()
+                .oauth2Login()
+                    .userInfoEndpoint()
+                    .userService(customOAuth2UserService);
+
+        return http.build();
+    }
+}
