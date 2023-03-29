c1e179cadd29e9eac1a4428f048730e946cdfbff
Woosung_Kim <sibu2005@naver.com>
2023-03-29 23:07:53 +0900

    네이버 로그인 추가

--- src/main/java/com/woosung/prj/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/woosung/prj/springboot/config/auth/dto/OAuthAttributes.java
@@ -24,11 +24,27 @@ public class OAuthAttributes {
         this.picture = picture;
     }
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
 
-    public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+    }
+
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
