a7c87374d27bc2f15c05cfeb238e95c1a9fc0297
Woosung_Kim <sibu2005@naver.com>
2023-03-31 10:49:32 +0900

    배포서버 테스트 실패에 대한 수정

--- src/main/java/com/woosung/prj/springboot/config/JpaConfig.java
+++ src/main/java/com/woosung/prj/springboot/config/JpaConfig.java
@@ -1,9 +1,9 @@
-package com.woosung.prj.springboot;
+package com.woosung.prj.springboot.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
-public class JpaAuditingConfiguration {
+public class JpaConfig {
 }
